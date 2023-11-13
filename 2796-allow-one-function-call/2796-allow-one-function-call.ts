edb3be16d9faa7de9e227bbb27146f1d77d01d80
정수원 <flowerneco@gmail.com>
2023-11-13 13:29:02 +0900

    Time: 57 ms (44.84%), Space: 43.1 MB (19.49%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,22 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let flag = true;
+    return function(...args){
+        if(flag){
+            flag = false;
+            const res = fn(...args);
+            return res;    
+        }
+        else return undefined;   
+    }
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
